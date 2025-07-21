216e848f7ef48944a4279116651f4647fa5908c3
vanosaur <vanirudra0914@gmail.com>
2025-07-21 16:15:36 +0530

    Fixing icons visibility

--- app/utils/fixLeafletIcon.js
+++ app/utils/fixLeafletIcon.js
@@ -1,15 +1,11 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-
-// Fix Leaflet's default icon paths
+// Fix for marker icons in Vercel deployment
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
+  iconRetinaUrl: '/images/marker-icon-2x.png',
+  iconUrl: '/images/marker-icon.png',
+  shadowUrl: '/images/marker-shadow.png',
 });
