726268754ff721bee8ec45ff0d06d54764d52e86
vanosaur <vanirudra0914@gmail.com>
2025-07-21 13:00:40 +0530

    FadeIn effect

--- app/properties/page.js
+++ app/properties/page.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react';
 import Properties from 'app/components/Properties/PropertiesPage'
 import PriceFilter from "../components/Properties/PriceFilter";
 
 
 function page() {
   return (
-    <div>
-      <Properties/>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Properties />
+    </Suspense>
   )
 }
 
